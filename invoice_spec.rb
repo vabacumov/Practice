0d6124e67d28fcb89f9b7e36149f6d06ef0acf5f
Chris Ginter <ginteruf@gmail.com>
2014-11-12 09:31:37 -0800

    Specs passing

--- invoice_spec.rb
+++ invoice_spec.rb
@@ -1,31 +1,35 @@
 require 'rspec'
 require_relative 'invoice.rb'
 
-describe Invoice, '#sales_tax' do
-  describe 'in California' do
-    let(:invoice) { Invoice.new subtotal: 100, state: 'CA' }
-    it { expect(invoice.sales_tax).to eq 10 }
+describe Invoice do
+  let(:invoice) { Invoice.new subtotal: 100 }
+
+  before { invoice.state = 'CA' }
+
+  describe '#subtotal' do
+    it { expect(invoice.subtotal).to eq 100 }
   end
 
-  describe 'in Florida' do
-    let(:invoice) { Invoice.new subtotal: 100, state: 'FL' }
-    it { expect(invoice.sales_tax).to eq 7 }
-  end
-end
+  describe '#sales_tax' do
+    it 'is 10% in California' do
+      expect(invoice.sales_tax).to eq 10
+    end
 
-describe Invoice, '#service_fee' do
-  describe 'when subtotal is less than $5000' do
-    let(:invoice) { Invoice.new subtotal: 100, state: 'CA' }
-    it { expect(invoice.service_fee).to eq 25 }
+    it 'is 7% in Florida' do
+      invoice.state = 'FL'
+      expect(invoice.sales_tax).to eq 7
+    end
   end
 
-  describe 'when subtotal is at least $5000' do
-    let(:invoice) { Invoice.new subtotal: 5000, state: 'CA' }
-    it { expect(invoice.service_fee).to eq 5000 }
+  describe '#service_fee' do
+    it 'is 20%' do
+      expect(invoice.service_fee).to eq 25
+    end
   end
-end
 
-describe Invoice, '#total' do
-  let(:invoice) { Invoice.new subtotal: 100, state: 'CA' }
-  it { expect(invoice.total).to eq 135 }
+  describe '#total' do
+    it 'includes sales tax and service fee' do
+      expect(invoice.total).to eq 135
+    end
+  end
 end
